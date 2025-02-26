6c2665e23eab4757f95b677d10b573fd094a2b75
IntoYourdreamS <DemonBubble24@gmail.com>
2025-02-26 11:09:21 +0700

    banyak coeg

--- src/Database/smart.sql
+++ src/Database/smart.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8111
--- Generation Time: Feb 23, 2025 at 01:38 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 26 Feb 2025 pada 03.22
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,110 +18,103 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `tokobusiti`
+-- Database: `smart`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `detail_pembelian`
+-- Struktur dari tabel `detail_pembelian`
 --
 
 CREATE TABLE `detail_pembelian` (
-  `id_pembelian` int(11) DEFAULT NULL,
-  `id_produk` int(11) DEFAULT NULL,
+  `id_pembelian` varchar(8) DEFAULT NULL,
+  `id_produk` varchar(8) DEFAULT NULL,
+  `nama_produk` varchar(25) NOT NULL,
   `jumlah` int(11) DEFAULT NULL,
-  `harga_satuan` decimal(10,2) DEFAULT NULL,
-  `total` decimal(10,2) DEFAULT NULL,
-  `create_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `id_detail_pembelian` int(11) NOT NULL,
-  `id_supplier` int(11) DEFAULT NULL
+  `harga_jual` int(25) DEFAULT NULL,
+  `harga_beli` int(25) NOT NULL,
+  `kategori` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `detail_penjualan`
+-- Struktur dari tabel `detail_penjualan`
 --
 
 CREATE TABLE `detail_penjualan` (
-  `id_penjualan` int(11) DEFAULT NULL,
-  `id_produk` int(11) DEFAULT NULL,
+  `id_penjualan` varchar(8) DEFAULT NULL,
+  `id_produk` varchar(8) DEFAULT NULL,
+  `kategori` varchar(15) NOT NULL,
   `jumlah` int(11) DEFAULT NULL,
   `harga_satuan` decimal(10,2) DEFAULT NULL,
-  `total` decimal(10,2) DEFAULT NULL,
-  `date` date NOT NULL DEFAULT current_timestamp(),
-  `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `id_detail_penjualan` int(11) NOT NULL
+  `subtotal` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `karyawan`
+-- Struktur dari tabel `karyawan`
 --
 
 CREATE TABLE `karyawan` (
-  `nama_karyawan` varchar(255) DEFAULT NULL,
-  `telepon` varchar(20) DEFAULT NULL,
-  `date` date NOT NULL DEFAULT current_timestamp(),
-  `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `id_karyawan` int(11) NOT NULL
+  `id_karyawan` varchar(8) NOT NULL,
+  `nama_karyawan` varchar(25) DEFAULT NULL,
+  `alamat` text NOT NULL,
+  `no_telp` int(9) NOT NULL,
+  `password` varchar(25) DEFAULT NULL,
+  `tanggal_masuk` timestamp NOT NULL DEFAULT current_timestamp(),
+  `RFID` int(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pembelian`
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` bigint(20) UNSIGNED NOT NULL,
+  `nama_kategori` varchar(255) DEFAULT NULL,
+  `create_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembelian`
 --
 
 CREATE TABLE `pembelian` (
-  `id_karyawan` int(11) DEFAULT NULL,
-  `total_harga` decimal(10,2) DEFAULT NULL,
-  `date` date NOT NULL DEFAULT current_timestamp(),
-  `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `id_pembelian` int(11) NOT NULL,
-  `id_supplier` int(11) DEFAULT NULL
+  `id_pembelian` varchar(8) NOT NULL,
+  `id_karyawan` varchar(8) DEFAULT NULL,
+  `id_supplier` varchar(8) DEFAULT NULL,
+  `tanggal` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pengeluaran`
---
-
-CREATE TABLE `pengeluaran` (
-  `nama_pengeluaran` varchar(255) NOT NULL,
-  `jumlah` int(11) NOT NULL,
-  `harga` decimal(10,2) NOT NULL,
-  `date` date NOT NULL DEFAULT current_timestamp(),
-  `id_pengeluaran` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `penjualan`
+-- Struktur dari tabel `penjualan`
 --
 
 CREATE TABLE `penjualan` (
-  `id_karyawan` int(11) DEFAULT NULL,
-  `id_pelanggan` int(11) DEFAULT NULL,
+  `id_penjualan` varchar(8) NOT NULL,
+  `id_karyawan` varchar(8) DEFAULT NULL,
   `tanggal` date DEFAULT NULL,
-  `total_harga` decimal(10,2) DEFAULT NULL,
-  `date` date NOT NULL DEFAULT current_timestamp(),
-  `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `id_penjualan` int(11) NOT NULL
+  `total_harga` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `produk`
+-- Struktur dari tabel `produk`
 --
 
 CREATE TABLE `produk` (
+  `id_produk` bigint(20) UNSIGNED NOT NULL,
   `nama_produk` varchar(255) DEFAULT NULL,
   `harga` decimal(10,2) DEFAULT NULL,
   `stok` int(11) DEFAULT NULL,
@@ -129,39 +122,34 @@ CREATE TABLE `produk` (
   `id_supplier` int(11) DEFAULT NULL,
   `barcode` varchar(50) DEFAULT NULL,
   `create_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `id_produk` int(11) NOT NULL
+  `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `retur_pembelian`
+-- Struktur dari tabel `retur_penjualan`
 --
 
-CREATE TABLE `retur_pembelian` (
-  `id_karyawan` int(11) DEFAULT NULL,
-  `id_pembelian` int(11) DEFAULT NULL,
+CREATE TABLE `retur_penjualan` (
+  `id_retur_penjualan` varchar(8) NOT NULL,
+  `id_penjualan` varchar(8) DEFAULT NULL,
   `tanggal_retur` date DEFAULT NULL,
-  `alasan` varchar(255) DEFAULT NULL,
-  `date` date NOT NULL DEFAULT current_timestamp(),
-  `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `id_retur_pembelian` int(11) NOT NULL
+  `alasan` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `supplier`
+-- Struktur dari tabel `supplier`
 --
 
 CREATE TABLE `supplier` (
+  `id_supplier` varchar(8) NOT NULL,
   `nama_supplier` varchar(255) DEFAULT NULL,
   `alamat` varchar(255) DEFAULT NULL,
   `telepon` varchar(20) DEFAULT NULL,
-  `date` date NOT NULL DEFAULT current_timestamp(),
-  `update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `id_supplier` int(11) NOT NULL
+  `create_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -169,116 +157,77 @@ CREATE TABLE `supplier` (
 --
 
 --
--- Indexes for table `detail_pembelian`
+-- Indeks untuk tabel `detail_pembelian`
 --
 ALTER TABLE `detail_pembelian`
-  ADD PRIMARY KEY (`id_detail_pembelian`);
+  ADD KEY `id_pembelian` (`id_pembelian`,`id_produk`);
 
 --
--- Indexes for table `detail_penjualan`
+-- Indeks untuk tabel `detail_penjualan`
 --
 ALTER TABLE `detail_penjualan`
-  ADD PRIMARY KEY (`id_detail_penjualan`);
+  ADD KEY `id_penjualan` (`id_penjualan`),
+  ADD KEY `id_produk` (`id_produk`);
 
 --
--- Indexes for table `karyawan`
+-- Indeks untuk tabel `karyawan`
 --
 ALTER TABLE `karyawan`
   ADD PRIMARY KEY (`id_karyawan`);
 
 --
--- Indexes for table `pembelian`
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `pembelian`
 --
 ALTER TABLE `pembelian`
-  ADD PRIMARY KEY (`id_pembelian`);
+  ADD PRIMARY KEY (`id_pembelian`),
+  ADD KEY `id_karyawan` (`id_karyawan`,`id_supplier`);
 
 --
--- Indexes for table `pengeluaran`
---
-ALTER TABLE `pengeluaran`
-  ADD PRIMARY KEY (`id_pengeluaran`);
-
---
--- Indexes for table `penjualan`
+-- Indeks untuk tabel `penjualan`
 --
 ALTER TABLE `penjualan`
-  ADD PRIMARY KEY (`id_penjualan`);
+  ADD PRIMARY KEY (`id_penjualan`),
+  ADD KEY `id_karyawan` (`id_karyawan`);
 
 --
--- Indexes for table `produk`
+-- Indeks untuk tabel `produk`
 --
 ALTER TABLE `produk`
   ADD PRIMARY KEY (`id_produk`);
 
 --
--- Indexes for table `retur_pembelian`
+-- Indeks untuk tabel `retur_penjualan`
 --
-ALTER TABLE `retur_pembelian`
-  ADD PRIMARY KEY (`id_retur_pembelian`);
+ALTER TABLE `retur_penjualan`
+  ADD PRIMARY KEY (`id_retur_penjualan`);
 
 --
--- Indexes for table `supplier`
+-- Indeks untuk tabel `supplier`
 --
 ALTER TABLE `supplier`
   ADD PRIMARY KEY (`id_supplier`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `detail_pembelian`
+-- AUTO_INCREMENT untuk tabel `kategori`
 --
-ALTER TABLE `detail_pembelian`
-  MODIFY `id_detail_pembelian` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `detail_penjualan`
---
-ALTER TABLE `detail_penjualan`
-  MODIFY `id_detail_penjualan` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `karyawan`
---
-ALTER TABLE `karyawan`
-  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pembelian`
---
-ALTER TABLE `pembelian`
-  MODIFY `id_pembelian` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pengeluaran`
---
-ALTER TABLE `pengeluaran`
-  MODIFY `id_pengeluaran` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `penjualan`
---
-ALTER TABLE `penjualan`
-  MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `produk`
+-- AUTO_INCREMENT untuk tabel `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `retur_pembelian`
---
-ALTER TABLE `retur_pembelian`
-  MODIFY `id_retur_pembelian` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `supplier`
---
-ALTER TABLE `supplier`
-  MODIFY `id_supplier` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_produk` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
